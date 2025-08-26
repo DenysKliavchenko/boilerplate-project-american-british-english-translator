b72d7d11b80d9f5b920b143cde8661eb3d6ab5f0
DenysKliavchenko <d.klyavchenko@gmail.com>
2025-08-26 12:14:05 +0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,8 +5,77 @@ const server = require('../server.js');
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
-
 suite('Functional Tests', () => {
+
+  test('Translation with text and locale fields: POST /api/translate', function (done) {
+    chai.request(server)
+      .post('/api/translate')
+      .send({ text: 'Mangoes are my favorite fruit.', locale: 'american-to-british' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'text');
+        assert.property(res.body, 'translation');
+        assert.equal(res.body.text, 'Mangoes are my favorite fruit.');
+        assert.include(res.body.translation, '<span class="highlight">favourite</span>');
+        done();
+      });
+  });
+
+  test('Translation with text and invalid locale field: POST /api/translate', function (done) {
+    chai.request(server)
+      .post('/api/translate')
+      .send({ text: 'text', locale: 'spanish-to-english' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, { error: 'Invalid value for locale field' });
+        done();
+      });
+  });
+
+  test('Translation with missing text field: POST /api/translate', function (done) {
+    chai.request(server)
+      .post('/api/translate')
+      .send({ locale: 'american-to-british' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, { error: 'Required field(s) missing' });
+        done();
+      });
+  });
+
+  test('Translation with missing locale field: POST /api/translate', function (done) {
+    chai.request(server)
+      .post('/api/translate')
+      .send({ text: 'Hello' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, { error: 'Required field(s) missing' });
+        done();
+      });
+  });
+
+  test('Translation with empty text: POST /api/translate', function (done) {
+    chai.request(server)
+      .post('/api/translate')
+      .send({ text: '', locale: 'american-to-british' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, { error: 'No text to translate' });
+        done();
+      });
+  });
+
+  test('Translation with text that needs no translation: POST /api/translate', function (done) {
+    chai.request(server)
+      .post('/api/translate')
+      .send({ text: 'I love programming.', locale: 'american-to-british' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'text');
+        assert.property(res.body, 'translation');
+        assert.equal(res.body.translation, 'Everything looks good to me!');
+        done();
+      });
+  });
 
 });
